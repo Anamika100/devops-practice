7a88d82e487bdf23fba708ccfa1de01c8bf7dc82
Anamika100 <anamikachoudhary128@gmail.com>
2025-07-28 12:15:27 +0530

    Added helloworld.java for Task 2

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+package devops-practice;
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to DevOps practice!");
+    }
+}
+
+
